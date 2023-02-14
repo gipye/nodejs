5489f19d1b7362abbf4411484e22fc4013cca8fe
gipye <gipyong7@gmail.com>
2023-02-15 00:27:09 +0900

    UserStorage.getUser() 메소드 수정

--- models/UserStorage.js
+++ models/UserStorage.js
@@ -11,6 +11,9 @@ class UserStorage {
         const temp = {};
         let i = 0;
 
+        if(property === undefined || property.length === 0) {
+            return users;
+        }
         for(i = 0; i < property.length; i++) {
             if(users.hasOwnProperty(property[i])) {
                 temp[property[i]] = users[property[i]];
